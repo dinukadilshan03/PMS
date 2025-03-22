f5842cabff8962ffe1aa1e837b6c15b6d0f5fdde
sahan <edu.sahan@gmail.com>
2025-03-23 01:13:58 +0530

    Finally updated one 23/3/25

--- frontend/src/app/page.tsx
+++ frontend/src/app/page.tsx
@@ -1,10 +1,10 @@
-import AlbumsShow from "@/app/Album-Portfolio/pages/AlbumsShow";
+import UploadAlbumPage from "@/app/Album-Portfolio/pages/UploadAlbumPage";
 
 
 export default function Home() {
   return (
    <div>
-        <AlbumsShow />
+        <UploadAlbumPage />
    </div>
   );
 }
