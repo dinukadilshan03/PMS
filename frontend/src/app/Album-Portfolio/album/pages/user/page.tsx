078805cb0da5202ce22e5c5bfe05445c6fb02ccd
sahan <edu.sahan@gmail.com>
2025-04-21 01:10:47 +0530

    User view of all albums and portfolios complete. but want to add more tailwind

---
+++ frontend/src/app/Album-Portfolio/album/pages/user/page.tsx
@@ -0,0 +1,10 @@
+import AlbumListUser from "@/app/Album-Portfolio/album/components/AlbumListUser";
+
+export default function AlbumListUserPage(){
+
+    return(
+        <div>
+            <AlbumListUser />
+        </div>
+    )
+}
